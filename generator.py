ef769c3a015a7b76eb83b7b822a9f153a2d312f0
akos1106 <78740597+akos1106@users.noreply.github.com>
2022-09-27 11:28:13 +0200

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,45 +1,154 @@
-from data.basic.model import Person
-from faker import Faker
+# from data.basic.model import Person
+# from faker import Faker
+# import random
+#
+#
+# def generate_people(n: int, male_ratio: float = 0.5,
+#                     locale: str = "en_US", unique: bool =False) -> list[Person]:
+#     fake = Faker(locale)
+#     if unique:
+#         fake=fake.unique
+#
+#
+#     people = []
+#     for i in range(1, n + 1):
+#         male = random.random() < male_ratio  # igaz vagy hamis
+#
+#         person = Person(
+#             f"P{str(i).zfill(6)}",  # id
+#             fake.name_male() if male else fake.name_female(),  # név
+#             random.randint(18, 100),  # kor
+#             male  # nem (boolean)
+#         )
+#         people.append(person)
+#
+#     return people  # vagy ez vagy a következő 9 sor
+#
+#     """return [    #listás comprihention(?)
+#         Person(
+#             f"P{str(i).zfill(6)}",      # id
+#             fake.name(),                # név
+#             random.randint(18, 100),    # kor
+#             random.randint(0, 1) == 1   # nem (boolean)
+#         )
+#         for i in range(1,n+1)
+#     ]"""
+#
+#
+# def main() -> None:
+#     people = generate_people(10, 0.7, locale="hu_HU")
+#     for person in people:
+#         print(person)
+#
+#
+# if __name__ == "__main__":
+#     main()
+
+
+# tanáré
 import random
+from faker import Faker
+from data.basic.model import Person, Car, Airport
+from faker_vehicle import VehicleProvider
+from faker_airtravel import AirTravelProvider
 
 
-def generate_people(n: int, male_ratio: float = 0.5,
-                    locale: str = "en_US", unique: bool =False) -> list[Person]:
+def generate_people(n: int, male_ratio: float = 0.5, locale: str = "en_US",
+                    unique: bool = False, min_age: int = 0, max_age: int = 100) -> list[Person]:
+    assert n > 0
+    assert 0 < male_ratio < 1
+    assert 0 <= min_age <= max_age
+
     fake = Faker(locale)
-    if unique:
-        fake=fake.unique
-
-
     people = []
-    for i in range(1, n + 1):
-        male = random.random() < male_ratio  # igaz vagy hamis
+    for i in range(n):
+        male = random.random() < male_ratio
+        generator = fake if not unique else fake.unique
+        people.append(Person(
+            "P-" + (str(i).zfill(6)),
+            generator.name_male() if male else generator.name_female(),
+            random.randint(min_age, max_age),
+            male))
 
-        person = Person(
-            f"P{str(i).zfill(6)}",  # id
-            fake.name_male() if male else fake.name_female(),  # név
-            random.randint(18, 100),  # kor
-            male  # nem (boolean)
+    return people
+
+
+def generate_cars(n: int,
+                  automatic_ratio: float = 0.2,
+                  locale: str = "hu_HU",
+                  unique: bool = False,
+                  min_year: int = 1950,
+                  max_year: int = 2022) -> list[Car]:
+    assert n >= 1
+    assert 0 <= automatic_ratio <= 1
+    assert min_year >= 1950
+    assert min_year <= max_year <= 2022
+
+    fake_plate = Faker(locale)
+    fake_plate.add_provider(VehicleProvider)  # ha community providert használsz
+
+    fake_type = Faker(locale)
+    fake_type.add_provider(VehicleProvider)
+
+    if unique:
+        fake_plate = fake_plate.unique
+
+    cars = []
+    for i in range(n):
+        car = Car(
+            fake_plate.license_plate(),
+            fake_type.vehicle_make(),
+            random.randint(min_year, max_year),
+            random.random() < automatic_ratio
         )
-        people.append(person)
 
-    return people  # vagy ez vagy a következő 9 sor
+        cars.append(car)
 
-    """return [    #listás comprihention(?)
-        Person(
-            f"P{str(i).zfill(6)}",      # id
-            fake.name(),                # név
-            random.randint(18, 100),    # kor
-            random.randint(0, 1) == 1   # nem (boolean)
-        )
-        for i in range(1,n+1)
-    ]"""
-
-
-def main() -> None:
-    people = generate_people(10, 0.7, locale="hu_HU")
-    for person in people:
-        print(person)
+    return cars
 
 
 if __name__ == "__main__":
-    main()
+    cars = generate_cars(20, unique=True)  # 50 különböző rendszámú
+    # for car in cars:
+    #     print(car)
+
+
+def generate_airports(n: int,
+                      country: str = None,
+                      unique: bool = False,
+                      attempts: int = None) -> list[Airport]:
+    fake = Faker()
+    fake.add_provider(AirTravelProvider)
+
+    airports = []
+
+    for i in range(n if attempts is None else attempts):
+
+        values = fake.airport_object()
+        if values["icao"] == "":
+            continue
+
+        if country is not None and values["country"] != country:
+            continue
+
+        if len(airports) == n:
+            break
+
+        airport = Airport(
+            values["icao"],
+            values["airport"],
+            values["city"],
+            values["state"],
+            values["country"]
+        )
+
+
+        if airport not in airports or not unique:
+            airports.append(airport)
+    return airports
+
+
+if __name__ == "__main__":
+    airports = generate_airports(5, attempts=10000, country="Germany", unique=True)
+    for air in airports:
+        print(air)
--- model.py
+++ model.py
@@ -1,12 +1,75 @@
-#@total_ordering
+# #@total_ordering
+# class Person:
+#     id: str
+#     name: str
+#     age: int
+#     male: bool
+#     def __init__(self, id: str, name: str, age: int, male: bool) -> None:
+#         # milyen értéket ad vissza a metódus (None,Str,Bool,Int)
+#         self.id = id
+#         self.name = name
+#         self.age = age
+#         self.male = male
+#
+#     def __str__(self) -> str:
+#         return "#{0}: {1} ({2}, {3})".format(self.id, self.name, self.age, self.male)
+#
+#     def __eq__(self, o: object) -> bool:
+#         return isinstance(o, Person) and self.id == o.id
+#
+#     def __ne__(self, other) -> bool:
+#         # de ezt nem kell, __eq__ alapján automatikusan csinálja
+#         return not self.__eq__(other)
+#
+#     def __hash__(self) -> int:
+#         return id.__hash__()
+#
+#     def __lt__(self, o: object) -> bool:
+#         if not isinstance(o, Person):
+#             return NotImplemented  # Persont nem lehet Car-ral összehasonlitani
+#
+#         return self.id < o.id
+#
+#     def __gt__(self, o: object) -> bool:
+#         if not isinstance(o, Person):
+#             return NotImplemented
+#         return self.id > o.id
+#
+#
+# # class Person2:
+# #     def __init__(self, id: str, name: str, age: int, male: bool) -> None:
+# #         self._id = id  # privátság (_) miatt nem kell konstruktor előtt megadni a tipusokat
+# #         self._name = name
+# #         self._age = age
+# #         self._male = male
+#
+#
+# if __name__ == "__main__":
+#     Person.id = "ID1"
+#     print(Person.__dict__)
+#     p1 = Person("P1", "Aladár", 18, True)
+#     print(p1.__dict__)
+#     p2 = Person("P1", "Aladár", 18, True)
+#
+#     # egyenlőek-e?
+#     print(p1 == p2)
+#     print(p1.__eq__(p2))
+#     print(Person.__eq__(p1, p2))
+
+
+#tanáré
+
+from functools import total_ordering
+
+
+@total_ordering
 class Person:
     id: str
     name: str
     age: int
     male: bool
 
-    def __init__(self, id: str, name: str, age: int, male: bool) -> None:
-        # milyen értéket ad vissza a metódus (None,Str,Bool,Int)
+    def __init__(self, id: str, name: str, age: int, male: bool = True) -> None:
         self.id = id
         self.name = name
         self.age = age
@@ -15,41 +78,76 @@ class Person:
     def __eq__(self, o: object) -> bool:
         return isinstance(o, Person) and self.id == o.id
 
-    def __ne__(self, other) -> bool:
-        # de ezt nem kell, __eq__ alapján automatikusan csinálja
-        return not self.__eq__(other)
+    def __str__(self) -> str:
+        return "#{0}: {1} ({2}, {3})".format(self.id, self.name, self.age, self.male)
 
     def __hash__(self) -> int:
-        return id.__hash__()
+        return self.id.__hash__()
 
     def __lt__(self, o: object) -> bool:
         if not isinstance(o, Person):
-            return NotImplemented  # Persont nem lehet Car-ral összehasonlitani
+            return NotImplemented
 
         return self.id < o.id
 
-    def __gt__(self, o: object) -> bool:
-        if not isinstance(o, Person):
+
+@total_ordering #az összes hátralévő nem definiált operátort csináljon meg a python
+class Car:
+    plate: str
+    type: str
+    year: int
+    automatic: bool
+
+    def __init__(self, plate: str, type: str, year: int, automatic=True) -> None:
+        self.plate = plate
+        self.type = type
+        self.year = year
+        self.automatic = automatic
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Car) and self.plate == o.plate
+
+    def __str__(self) -> str:
+        return "{0} ({1}, {2}): {3}".format(self.plate, self.type, self.year, self.automatic)
+
+    def __hash__(self) -> int:
+        return self.plate.__hash__()
+
+    def __lt__(self, o: object) -> bool:
+        if not isinstance(o, Car):
             return NotImplemented
-        return self.id > o.id
+
+        return self.plate < o.plate
 
 
-# class Person2:
-#     def __init__(self, id: str, name: str, age: int, male: bool) -> None:
-#         self._id = id  # privátság (_) miatt nem kell konstruktor előtt megadni a tipusokat
-#         self._name = name
-#         self._age = age
-#         self._male = male
+@total_ordering
+class Airport:
+    code: str
+    name: str
+    city: str
+    state: str
+    country: str
 
+    def __init__(self, code: str, name: str, city: str, state: str, country: str) -> None:
+        self.code = code
+        self.name = name
+        self.city = city
+        self.state = state
+        self.country = country
 
-if __name__ == "__main__":
-    Person.id = "ID1"
-    print(Person.__dict__)
-    p1 = Person("P1", "Aladár", 18, True)
-    print(p1.__dict__)
-    p2 = Person("P1", "Aladár", 18, True)
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Airport) and self.code == o.code
 
-    # egyenlőek-e?
-    print(p1 == p2)
-    print(p1.__eq__(p2))
-    print(Person.__eq__(p1, p2))
+    def __str__(self) -> str:
+        return "{code} ({name}): {city}, {state}, {country}".format(
+            code=self.code, name=self.name, city=self.city,
+            state=self.state, country=self.country)
+
+    def __hash__(self) -> int:
+        return self.code.__hash__()
+
+    def __lt__(self, o: object) -> bool:
+        if not isinstance(o, Airport):
+            return NotImplemented
+
+        return self.code < o.code
